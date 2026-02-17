0c42ca8364a3ea251f795ff855c15a849bf29ce2
Ajay <ajaysingh01103@gmail.com>
2026-02-17 14:46:43 +0530

    API types

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,2 +1,2 @@
-// Re-export API type from the API package
-export type { AppType } from "api"
+// Re-export API routes type from the API package for RPC client
+export type { ApiRoutes } from "api"
